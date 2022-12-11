b006f40132482d832c0a77724ae1e552361e39bf
geekris1 <200949970@qq.com>
2022-12-11 11:42:37 +0800

    chore:change port

--- src/node/server.ts
+++ src/node/server.ts
@@ -8,8 +8,8 @@ export function server(root: string) {
   const staticPath = join(ROOT, root, 'build')
   const assets = sirv(staticPath)
   polka().use(assets)
-    .listen(3000, err => {
+    .listen(2306, err => {
       if (err) throw err;
-      console.log('> Ready on http://127.0.0.1:1010 ~!');
+      console.log('> Ready on http://127.0.0.1:2306 ~!');
     });
 }
